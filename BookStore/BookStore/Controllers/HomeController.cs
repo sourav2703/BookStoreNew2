7580df0214cb93b844044d3f365e894dc4d2b6e9
hp <hp@DESKTOP-J482VSN>
2022-12-16 22:54:04 +0530

    data ko model se view me vej diya gya hai .. step 1

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Models;
+
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,29 @@ namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ViewResult GetAllBooks()
+        {
+            var data = list();
+            return View(data);
+        }
+        public ViewResult Index()
         {
             return View();
         }
+
+        private List<BookModel> list()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){Id=1,Price=101,Description="This is demo book", Title="Dot Net"},
+                new BookModel(){Id=2,Price=102,Description="This is demo book", Title="MVC"},
+                new BookModel(){Id=3,Price=103,Description="This is demo book", Title="Java"},
+                new BookModel(){Id=4,Price=104,Description="This is demo book", Title="c++"}
+            };
+
+        }
+
+
+
     }
 }
---
+++ BookStore/BookStore/Models/BookModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Controllers;
+
+namespace BookStore.Models
+{
+    public class BookModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Id { get; set; }
+    }
+}
