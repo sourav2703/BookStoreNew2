b5386c30e8af1df197b536bf79bb344027c1ff9f
hp <hp@DESKTOP-J482VSN>
2022-12-18 00:07:10 +0530

    ab, one book wala page show kr rha hai. ache se !

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -21,9 +21,9 @@ namespace BookStore.Controllers
             return View(data);
 
         }
-        public ViewResult GetOneBook()
+        public ViewResult GetOneBook(int id)
         {
-            var data = list();
+            var data = getonee(id);
             return View(data);
         }
        
@@ -36,15 +36,19 @@ namespace BookStore.Controllers
             return View();
         }
        
-
+        private BookModel getonee(int id)
+        {
+            var a = list();
+           return a.Where(x => x.Id == id).FirstOrDefault();
+        }
         private List<BookModel> list()
         {
             return new List<BookModel>()
             {
-                new BookModel(){Id=1,Price=101,Description="This is demo book", Title="Dot Net",Author="Sourav kr"},
-                new BookModel(){Id=2,Price=102,Description="This is demo book", Title="MVC",Author="Sourav kr ch"},
-                new BookModel(){Id=3,Price=103,Description="This is demo book", Title="Java" ,Author="Sourav kumar"},
-                new BookModel(){Id=4,Price=104,Description="This is demo book", Title="c++",Author="Sourav choudhary"}
+                new BookModel(){Id=1,Price=101,Description="This is demo book", Title="Dot Net",Author="Sourav kr",Category="Study",Language="English",PageCount=500},
+                new BookModel(){Id=2,Price=102,Description="This is demo book", Title="MVC",Author="Sourav kr ch",Category="Study",Language="English",PageCount=400},
+                new BookModel(){Id=3,Price=103,Description="This is demo book", Title="Java" ,Author="Sourav kumar",Category="Study",Language="English",PageCount=30},
+                new BookModel(){Id=4,Price=104,Description="This is demo book", Title="c++",Author="Sourav choudhary",Category="Study",Language="English",PageCount=5120}
             };
 
         }
--- BookStore/BookStore/Models/BookModel.cs
+++ BookStore/BookStore/Models/BookModel.cs
@@ -13,5 +13,8 @@ namespace BookStore.Models
         public int Price { get; set; }
         public int Id { get; set; }
         public string Author { get; set; }
+        public int PageCount { get; set; }
+        public string Language { get; set; }
+        public string Category { get; set; }
     }
 }
